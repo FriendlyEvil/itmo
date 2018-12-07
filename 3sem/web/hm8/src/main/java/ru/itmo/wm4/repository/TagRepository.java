a56f9b804350ccf486b7fc24207cea954d5c0b55
Кривопальцев Дмитрий <37186900+FriendlyEvil@users.noreply.github.com>
2018-12-07 12:56:12 +0300

    update hm 8

---
+++ 3sem/web/hm8/src/main/java/ru/itmo/wm4/repository/TagRepository.java
@@ -0,0 +1,13 @@
+package ru.itmo.wm4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.itmo.wm4.domain.Comment;
+import ru.itmo.wm4.domain.Tag;
+
+import java.util.List;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Tag findByName(String name);
+
+
+}
