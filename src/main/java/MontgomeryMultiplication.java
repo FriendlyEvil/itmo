f316b58f9436f81659b8f741d76f643cc515503a
Roman Smirnov <smirnovromal@yandex-team.ru>
2019-12-04 20:56:39 +0300

    gcd

--- src/main/java/MontgomeryMultiplication.java
+++ src/main/java/MontgomeryMultiplication.java
@@ -30,12 +30,16 @@ public class MontgomeryMultiplication {
         BigInteger r = BigInteger.valueOf(1).shiftLeft(n.bitLength() + 1);
         BigInteger[] r_inv = new BigInteger[1];
         BigInteger[] n_sh = new BigInteger[1];
+//        BigIntEuclidean calculate = BigIntEuclidean.calculate(r, n);
+
         Tuple gcd = gcd(r, n, r_inv, n_sh);
+//        r_inv[0] = calculate.x;
+//        n_sh[0] = calculate.y;
         n_sh[0] = n_sh[0].multiply(BigInteger.valueOf(-1));
         BigInteger t = a.multiply(b);
         BigInteger u = (t.add(t.multiply(n_sh[0]).mod(r).multiply(n))).divide(r);
         while (u.compareTo(n) >= 0) {
-            u = u.subtract(n);
+            u = u.mod(n);
         }
         return u;
     }
@@ -58,5 +62,45 @@ public class MontgomeryMultiplication {
         BigInteger[] x = new BigInteger[1];
         BigInteger[] y = new BigInteger[1];
         Tuple gcd = gcd(BigInteger.valueOf(3), BigInteger.valueOf(5), x, y);
+    }
+
+    public static class BigIntEuclidean
+    {
+        public BigInteger x, y, gcd;
+
+        private BigIntEuclidean()
+        {
+        }
+
+        public static BigIntEuclidean calculate(BigInteger a, BigInteger b)
+        {
+            BigInteger x = BigInteger.ZERO;
+            BigInteger lastx = BigInteger.ONE;
+            BigInteger y = BigInteger.ONE;
+            BigInteger lasty = BigInteger.ZERO;
+            while (!b.equals(BigInteger.ZERO))
+            {
+                BigInteger[] quotientAndRemainder = a.divideAndRemainder(b);
+                BigInteger quotient = quotientAndRemainder[0];
+
+                BigInteger temp = a;
+                a = b;
+                b = quotientAndRemainder[1];
+
+                temp = x;
+                x = lastx.subtract(quotient.multiply(x));
+                lastx = temp;
+
+                temp = y;
+                y = lasty.subtract(quotient.multiply(y));
+                lasty = temp;
+            }
+
+            BigIntEuclidean result = new BigIntEuclidean();
+            result.x = lastx;
+            result.y = lasty;
+            result.gcd = a;
+            return result;
+        }
     }
 }
