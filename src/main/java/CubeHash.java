faf0896f3b8959ea1b518fb3b754c881b84d9137
Roman Smirnov <smirnovromal@yandex-team.ru>
2019-12-17 13:00:25 +0300

    init2.0

--- src/main/java/CubeHash.java
+++ src/main/java/CubeHash.java
@@ -1,7 +1,18 @@
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-@Value
+@Data
+@AllArgsConstructor
 public class CubeHash {
     public static void main(String[] args) {
+        CubeHash cubeHash = new CubeHash(16, 32, 512, 32);
+        int b = cubeHash.b;
     }
+
+    int r; //rounds count
+    int b; //block size in bytes
+    int h; //hash size in bits
+    int f; //final rounds count
+
+
 }
